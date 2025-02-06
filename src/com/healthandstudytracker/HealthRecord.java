659006743ec9a5262299de31ec19114848d55b2f
shahana <shahanatarannum6@gmail.com>
2025-02-06 21:37:45 +0530

    added the health record

--- src/com/healthandstudytracker/HealthRecord.java
+++ src/com/healthandstudytracker/HealthRecord.java
@@ -1,4 +1,39 @@
 package com.healthandstudytracker;
 
-public class HealthRecord {
-}
+    public class HealthRecord {
+        private int dailySteps;
+        private int sleepHours;
+        private int exerciseDuration; // In minutes
+
+        public HealthRecord() {
+            this.dailySteps = 0;
+            this.sleepHours = 0;
+            this.exerciseDuration = 0;
+        }
+
+        public void addSteps(int steps) {
+            dailySteps += steps;
+        }
+
+        public void addSleepHours(int hours) {
+            sleepHours += hours;
+        }
+
+        public void addExerciseDuration(int minutes) {
+            exerciseDuration += minutes;
+        }
+
+        // Getters
+        public int getDailySteps() {
+            return dailySteps;
+        }
+
+        public int getSleepHours() {
+            return sleepHours;
+        }
+
+        public int getExerciseDuration() {
+            return exerciseDuration;
+        }
+    }
+
