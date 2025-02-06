24a646c98ff7871c14a2f8e9d6ac2b30dc64a579
shahana <shahanatarannum6@gmail.com>
2025-02-06 22:51:19 +0530

    goal set

--- src/com/healthandstudytracker/Goal.java
+++ src/com/healthandstudytracker/Goal.java
@@ -1,4 +1,24 @@
 package com.healthandstudytracker;
 
-public class Goal {
-}
+    public class Goal {
+        private String description;
+        private boolean achieved;
+
+        public Goal(String description) {
+            this.description = description;
+            this.achieved = false;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public boolean isAchieved() {
+            return achieved;
+        }
+
+        public void setAchieved(boolean achieved) {
+            this.achieved = achieved;
+        }
+    }
+
